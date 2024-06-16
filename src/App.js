794890c566d8b018065e125b3682e7196f1cf145
abkabbani <abkabbani@gmail.com>
2024-06-16 14:59:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { AppProvider } from './context/AppContext';
+//Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
-import RemainingBudget from './components/Remaining';
+import CurrencyDropdown from './components/CurrencyDropdown';
+import { AppProvider } from './context/AppContext';
 
 const App = () => {
     return (
@@ -18,27 +21,34 @@ const App = () => {
                         <Budget />
                     </div>
                     <div className='col-sm'>
-                        <RemainingBudget />
+                        <Remaining />
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
+                    <div className='col-sm'>
+                        <CurrencyDropdown />
+                    </div>
+
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
+
                 </div>
-                <h3 className='mt-3'>Change allocation</h3>
+                <h3 className='mt-3'>Change Allocation</h3>
                 <div className='row mt-3'>
+                    {/* <div className='col-sm'>
+                        <ExpenseItem />
+                    </div> */}
                     <div className='col-sm'>
-                        <AllocationForm/>
+                        <AllocationForm />
                     </div>
                 </div>
             </div>
         </AppProvider>
     );
 };
-
 export default App;
